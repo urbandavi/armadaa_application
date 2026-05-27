549182dd6c3407ca09c5e7a9b2d3f2bcb0a42011
urbandavi <urbandavidpeter@turr.hu>
2026-05-27 12:54:47 +0200

    Add project files.

---
+++ armadaa_application/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace armadaa_application
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
