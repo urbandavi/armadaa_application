2c236632cc6473d665ee4275998dc904ebd43fcb
urbandavi <urbandavidpeter@turr.hu>
2026-06-10 12:16:42 +0200

    login logick

--- armadaa_application/MainWindow.xaml.cs
+++ armadaa_application/MainWindow.xaml.cs
@@ -24,7 +24,36 @@ namespace armadaa_application
 
         private void loginBtn_Click(object sender, RoutedEventArgs e)
         {
+            string userName = loginUserText.Text;
+            string passwordHash = PasswordHelper.HashPassword(loginPasswordText.Password);
 
+            if (!string.IsNullOrEmpty(loginUserText.Text) || !string.IsNullOrEmpty(loginPasswordText.Password))
+            {
+                using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
+                {
+                    var user = connection.Table<Felhasznalo>().FirstOrDefault(u => u.FelhasznaloNev == userName);
+
+                    //Ha van ilyen felhasználó
+                    if (user != null)
+                    {
+                        // jelszóellenőrzés
+                        if (user.Jelszo == passwordHash)
+                        {
+                            MainWindow mainWindow = new MainWindow();
+                            mainWindow.Show();
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Belépés megtagadva! ");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Belépés megtagadva! ");
+                    }
+                }
+            }
         }
 
         private void toRegButton_Click(object sender, RoutedEventArgs e)
