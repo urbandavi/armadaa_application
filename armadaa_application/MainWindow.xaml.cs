36264d8b304f4722a895c2276468372fa8a55309
urbandavi <urbandavidpeter@turr.hu>
2026-06-10 13:18:16 +0200

    This is not good

--- armadaa_application/MainWindow.xaml.cs
+++ armadaa_application/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using armadaa_application.Recources;
+﻿using armadaa_application.Model;
+using armadaa_application.Recources;
+using eKreta.Services;
+using SQLite;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,35 +27,13 @@ namespace armadaa_application
 
         private void loginBtn_Click(object sender, RoutedEventArgs e)
         {
-            string userName = loginUserText.Text;
-            string passwordHash = PasswordHelper.HashPassword(loginPasswordText.Password);
 
-            if (!string.IsNullOrEmpty(loginUserText.Text) || !string.IsNullOrEmpty(loginPasswordText.Password))
+            string userName = usernameTbx.Text;
+            string passwordHash = PasswordHelper.HashPassword(passwordTbx.Password);
+
+            using(SQLiteConnection connection = new SQLiteConnection(App.databasePath))
             {
-                using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
-                {
-                    var user = connection.Table<Felhasznalo>().FirstOrDefault(u => u.FelhasznaloNev == userName);
-
-                    //Ha van ilyen felhasználó
-                    if (user != null)
-                    {
-                        // jelszóellenőrzés
-                        if (user.Jelszo == passwordHash)
-                        {
-                            MainWindow mainWindow = new MainWindow();
-                            mainWindow.Show();
-                            this.Close();
-                        }
-                        else
-                        {
-                            MessageBox.Show("Belépés megtagadva! ");
-                        }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Belépés megtagadva! ");
-                    }
-                }
+               
             }
         }
 
--- armadaa_application/Recources/registration.xaml.cs
+++ armadaa_application/Recources/registration.xaml.cs
@@ -38,17 +38,25 @@ namespace armadaa_application.Recources
             {
                 using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
                 {
+
+                    connection.CreateTable<User>();
                     var user = connection.Table<User>().FirstOrDefault(u => u.Username == userName);
 
                     //Ha van ilyen felhasználó
-                    if (user != null)
-                    {
+                    
+                        if (passwordHash == passwordAgainHash) { 
+                        
                         var UserRepo = new GenericRepository<User>(App.databasePath);
-                        var registreredUser = new User(userName, email, passwordHash);
-                    }
+                        var registreredUser = new User(passwordHash, userName, email);
+                        UserRepo.insert(registreredUser);
+                         Dashboard dashboard = new Dashboard();
+                            dashboard.Show();
+                            this.Close();
+                        }
+                    
                     else
                     {
-                        MessageBox.Show("Belépés megtagadva! ");
+                        MessageBox.Show("Regisztráció Sikeretelen");
                     }
                 }
             }
@@ -57,12 +65,14 @@ namespace armadaa_application.Recources
 
         private void toLoginBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
         }
 
         private void exit_Click(object sender, RoutedEventArgs e)
         {
-
+            App.Current.Shutdown();
         }
     }
 }
